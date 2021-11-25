cd52a5553babc3c154c591f544793de01af4f2b1
gbelda <gbelda56@gmail.com>
2021-11-25 11:56:46 +0100

    complete header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
-    <header>
-      <nav class="d-flex justify-between">
-        <img src="./assets/img/medical_logo_1x_light.png" alt="" />
-      </nav>
-    </header>
+    <site-header />
   </div>
 </template>
 
 <script>
+import SiteHeader from "./components/SiteHeader.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    SiteHeader,
+  },
 };
 </script>
 
 <style lang="scss">
-@import url(./assets/scss/common.scss);
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap");
+@import "./assets/scss/common.scss";
 </style>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,8 +1,92 @@
-<template></template>
+<template>
+  <header class="d-flex align-center">
+    <nav class="d-flex justify-between align-center">
+      <div class="logo">
+        <img src="../assets/img/medical_logo_1x_light.png" alt="" />
+      </div>
+      <div class="nav_options d-flex align-center">
+        <ul class="nav_index d-flex">
+          <li
+            v-for="page in pages"
+            :key="page.section"
+            :class="{ selected: page.selected }"
+          >
+            {{ page.section.toUpperCase() }}
+          </li>
+        </ul>
+        <button>MAKE APPOINTMENT</button>
+      </div>
+    </nav>
+  </header>
+</template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      pages: [
+        {
+          section: "home",
+          selected: true,
+        },
+        {
+          section: "about",
+          selected: false,
+        },
+        {
+          section: "departments",
+          selected: false,
+        },
+        {
+          section: "articles",
+          selected: false,
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
+<style lang='scss'>
+@import "../assets/scss/variables.scss";
+header {
+  background-color: transparent;
+  height: 90px;
+
+  nav {
+    width: 100%;
+    padding: 0px 5rem;
+    font-size: 0.8rem;
+
+    .logo {
+      width: 30%;
+
+      img {
+        width: 45%;
+      }
+    }
+
+    .nav_index {
+      color: white;
+      list-style: none;
+      margin-right: 3rem;
+      font-weight: 300;
+
+      li {
+        padding-left: 3rem;
+      }
+      .selected {
+        color: $highlight_primary;
+      }
+    }
+
+    button {
+      padding: 0.7rem 1.8rem;
+      background-color: $highlight_primary;
+      color: white;
+      border: none;
+      border-radius: 3px;
+    }
+  }
+}
 </style>
